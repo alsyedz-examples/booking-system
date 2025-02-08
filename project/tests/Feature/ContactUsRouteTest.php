fe60ec0a48a1aa1969f391abc6448188090b016b
Syed OG <syednaeem15191@gmail.com>
2025-02-08 17:59:10 +0000

    Update ContactUsRouteTest.php

--- project/tests/Feature/ContactUsRouteTest.php
+++ project/tests/Feature/ContactUsRouteTest.php
@@ -34,4 +34,24 @@ class ContactUsRouteTest extends TestCase
             'name="subject"',
         ], false);
     }
+
+    /**
+     * A test to see if you can submit contact-us form with errors from contact-us route.
+     */
+    public function test_contact_us_route_submits_contact_us_form_with_errors(): void
+    {
+        $response = $this->get('/contact-us');
+
+        $response->assertOk();
+    }
+
+    /**
+     * A test to see if you can submit contact-us form without errors from contact-us route.
+     */
+    public function test_contact_us_route_submits_contact_us_form_without_errors(): void
+    {
+        $response = $this->get('/contact-us');
+
+        $response->assertOk();
+    }
 }
